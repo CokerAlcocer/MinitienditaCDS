d6c469178b7bf89766de1b14be17603e677a6f0d
Angel Y. Alcocer Durán <20203tn066@utez.edu.mx>
2022-08-06 15:12:55 -0500

    06/08/2022 15:12

---
+++ js/app/productsController.js
@@ -0,0 +1,18 @@
+const app = angular.module('MinitienditaCDS', [])
+
+app.controller('productController', ['$scope', '$http', ($scope, $http) => {
+    $scope.products = []
+
+    $scope.getData = () => {
+        $http({
+            method: 'GET',
+            url: '../../assets/sources/data.json'
+        }).then(res => {
+            let {data} = res
+            for(let i = 0; i < data.length; i++){
+                data[i].id = i+1
+            }
+            $scope.products = data
+        })
+    }
+}])
