2217fe9e159f7b799f97b0dd9c38ca0bd6f2fa15
Angel Y. Alcocer Durán <20203tn066@utez.edu.mx>
2022-08-07 00:57:14 -0500

    06/08/2022 A

--- js/app/productsController.js
+++ js/app/productsController.js
@@ -6,7 +6,7 @@ app.controller('productController', ['$scope', '$http', ($scope, $http) => {
     $scope.getData = () => {
         $http({
             method: 'GET',
-            url: '../../assets/sources/data.json'
+            url: 'https://cokeralcocer.github.io/MinitienditaPososa/assets/sources/data.json'
         }).then(res => {
             let {data} = res
             for(let i = 0; i < data.length; i++){
